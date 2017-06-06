5af4964ea7c59bf2050dd189f87f00b1ced49a44
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2017-06-06 16:52:53 +1200

    Login into google sheets

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
+import Spreadsheet from './Spreadsheet.js'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    
+
     this.clientId = '826265862385-p41e559ccssujlfsf49ppmo0gktkf6co.apps.googleusercontent.com';
-    this.discoveryDocs = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-    this.scopes = "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly";
     this.spreadsheetId = "18uwYwUAVw0H5bhszMgAORmvAN2APxAtJI3FB-XH7Dzk";
   }
 
   render() {
-
-    const authorize = (<button>Authorize</button>);
-    const loggedIn = (<button>Sign Out</button>);
-
     return (
       <div className="app">
-        <div className="header">
+        <header>
           <h2>Expenses</h2>
-        </div>
-
+        </header>
+        <Spreadsheet clientId={this.clientId} spreadsheetId={this.spreadsheetId} />
       </div>
     );
   }
