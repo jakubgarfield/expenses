61f499b76457a76f21ffee94210ff9d4591b242c
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2017-06-09 11:08:33 +1200

    Change form to a controlled component with HTML markup validations

--- src/ExpenseForm.js
+++ src/ExpenseForm.js
@@ -6,41 +6,75 @@ class ExpenseForm extends Component {
 
     this.state = {
       expanded: false,
+      amount: 0,
+      description: '',
+      date: new Date().toISOString().slice(0, 10),
+      category: this.props.categories[0],
+      account: this.props.accounts[0],
+      isValid: false
     };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   toggle() {
     this.setState({ expanded: !this.state.expanded });
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    target.reportValidity();
+    this.setState({
+      [target.name]: value,
+      isValid: this.form.checkValidity(),
+    });
+  }
+
+  handleSubmit() {
+
+  }
+
   render() {
     if (this.state.expanded)
       return (
-        <form id="add-expense">
+        <form onSubmit={this.handleSubmit} ref={form => { this.form = form; }} noValidate>
           <fieldset>
             <legend>Add Expense</legend>
             <button onClick={() => this.toggle()}>Close</button>
 
-            <label htmlFor="amount">Amount</label>
-            <input id="amount" type="number" min="0.01" step="0.01" required />
+            <label>
+              Amount
+              <input name="amount" value={this.state.amount} onChange={this.handleInputChange} type="number" step="0.01" min="0" required />
+            </label>
 
-            <label htmlFor="description">Description</label>
-            <input id="description" type="text" required />
+            <label>
+              Description
+              <input name="description" value={this.state.description} onChange={this.handleInputChange} type="text" />
+            </label>
 
-            <label htmlFor="date">Date</label>
-            <input id="date" type="date" defaultValue={new Date().toISOString().slice(0, 10)} required />
+            <label>
+              Date
+              <input name="date" value={this.state.date} onChange={this.handleInputChange} type="date" required />
+            </label>
 
-            <label htmlFor="category">Category</label>
-            <select id="category" required>
-              {this.props.categories.map(category => <option value={category} key={category}>{category}</option>)}
-            </select>
+            <label>
+              Category
+              <select name="category" value={this.state.category} onChange={this.handleInputChange} required>
+                {this.props.categories.map(category => <option value={category} key={category}>{category}</option>)}
+              </select>
+            </label>
 
-            <label htmlFor="account">Account</label>
-            <select id="account" required>
-              {this.props.accounts.map(account => <option value={account} key={account}>{account}</option>)}              
-            </select>
+            <label>
+              Account
+              <select name="account" value={this.state.account} onChange={this.handleInputChange} required>
+                {this.props.accounts.map(account => <option value={account} key={account}>{account}</option>)}
+              </select>
+            </label>
 
-            <input type="submit" value="Add" />
+            <input type="submit" value="Add" disabled={!this.state.isValid} />
           </fieldset>
         </form>
       );
