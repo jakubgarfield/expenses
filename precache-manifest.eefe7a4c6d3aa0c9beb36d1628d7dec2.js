a1c3aca1002bc74ba6cf7f0e53b19bf84d8732e7
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2020-04-12 11:14:40 +1200

    Release

---
+++ precache-manifest.eefe7a4c6d3aa0c9beb36d1628d7dec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f832f13e5dde0ba302ce9948bbe19827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81b8fae81ab58f7798b9",
+    "url": "/static/css/2.bf123088.chunk.css"
+  },
+  {
+    "revision": "a3cd2624889146d4131b",
+    "url": "/static/css/main.d16eb657.chunk.css"
+  },
+  {
+    "revision": "81b8fae81ab58f7798b9",
+    "url": "/static/js/2.a0aecb11.chunk.js"
+  },
+  {
+    "revision": "f14083e6e3fa69b0fba330ce12367a6d",
+    "url": "/static/js/2.a0aecb11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3cd2624889146d4131b",
+    "url": "/static/js/main.7836d081.chunk.js"
+  },
+  {
+    "revision": "f31b34e9a7a60b9e51c0",
+    "url": "/static/js/runtime-main.e901c6d6.js"
+  }
+]);
